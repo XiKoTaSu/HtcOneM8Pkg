a749a08eaa69f37938222870b9831d4aa5418a36
XiKoTaSu_XKTS <3329989998@qq.com>
2026-06-26 18:13:41 +0800

    Update DTB file in boot image creation script

--- build_boot_images.sh
+++ build_boot_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat BootShim/BootShim.bin workspace/Build/HtcOneM8/DEBUG_CLANGDWARF/FV/MSM8974_UEFI.fd >ImageResources/HtcOneM8/bootpayload.bin
 
-./ImageResources/mkbootimg/mkbootimg.py --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dtb ImageResources/HtcOneM8/qcdt.img --ramdisk ImageResources/HtcOneM8/dummy -o ImageResources/HtcOneM8/uefi.img
+./ImageResources/mkbootimg/mkbootimg.py --kernel ImageResources/HtcOneM8/bootpayload.bin --base 0x00000000 --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --dtb ImageResources/HtcOneM8/virgo.dtb --ramdisk ImageResources/HtcOneM8/dummy -o ImageResources/HtcOneM8/uefi.img
 
 # Create an elf for loading with BootShim
 #llvm-objcopy -I binary -O elf32-littlearm --binary-architecture arm workspace/Build/HtcOneM8/DEBUG_CLANGDWARF/FV/MSM8974_UEFI.fd ImageResources/HtcOneM8/MSM8974_EFI.fd.elf
